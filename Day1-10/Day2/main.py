0ba74706004129e40d09f6967a3de715ca192a63
Aditya Kumar <adii.utsav@gmail.com>
2025-12-13 08:01:00 +0530

    Studied topics variables, comments, print, escape sequence characters

---
+++ Day1-10/Day2/main.py
@@ -0,0 +1,32 @@
+#Single line commnet
+# print("Hello, World!")
+
+"""This is a multi-line comment."""
+# print("This is a multi-line comment example.")
+
+# print("Aditya")
+# print(7)
+# print('Utsav')
+# print(7+8)
+
+# print("Name:\tAditya\nCourse:\tPython")
+
+# print("Hey Aditya",6,7,7,8,sep="-", end="!!!\n")
+# print("Adii")
+
+# x = 9
+# name = "Aditya"
+# print(x)
+# print(name)
+
+# x = 9
+# x = 8
+# print(x)
+
+
+# print
+# (type(7))
+# print(type("Aditya"))
+
+a, b, c = 1, 2, 3
+print(a, b, c)
