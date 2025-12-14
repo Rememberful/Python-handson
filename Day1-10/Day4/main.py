8086377f98d794b89707d3d55a204b4890bac778
Aditya Kumar <adii.utsav@gmail.com>
2025-12-14 18:24:05 +0530

    Studied topics named strings and slicing of the string

---
+++ Day1-10/Day4/main.py
@@ -0,0 +1,29 @@
+# f_name = "Aditya"
+# l_name = "Kumar"
+# print(f_name)
+# print(l_name)
+# apple = '"Aditya is a boy"'
+# print(apple)
+
+# f_name = input("Enter your first name: ")
+
+# message = '''Aditya 
+# Are you good?
+# Yes I am fine.'''
+# print(message)
+
+# name = "Aditya"
+# print(name[0])
+# print(name[1])
+# print(name[6])
+
+# sentence = "Aditya"
+# for char in sentence:
+#     print(char)
+
+# name = "Aditya,is"
+# print(name[0:4])
+# print(name[:4])
+
+name = "Aditya,is"
+print(name[:-1])
