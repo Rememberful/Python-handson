6991e6099c6e975d47b1cb47bab43b8a977eb31c
Aditya Kumar <adii.utsav@gmail.com>
2025-12-15 07:42:58 +0530

    Studied topics named string methods

---
+++ Day1-10/Day5/main.py
@@ -0,0 +1,54 @@
+# a = "Aditya is a"
+# print(a)
+# print(a.title()) #capitalizes first letter of each word
+# print(a.capitalize()) #capitalizes first letter of the string
+
+# name = "!!Aditya!!"
+# print(name)
+# print(name.strip("!"))  #removes specified characters from both ends
+# print(name.lstrip("!")) #removes specified characters from the left end
+# print(name.rstrip("!")) #removes specified characters from the right end
+
+# text = "I like Java, but Java is not so java"
+# print(text)
+# print(text.replace("Java", "Python"))
+
+# name = "Aditya"
+# print(name.index("d"))  #returns the index of the first occurrence of the specified value
+# print(name.find("z"))  #returns the index of the first occurrence of the specified value
+# print(name.index("z"))  #returns the index of the first occurrence of the specified value
+
+# a = "Aditya a"
+# print(a.count("a")) 
+# print(a.count(a)) 
+
+# name = "Aditya"
+# print(name.split())
+# print(name.split("i"))
+# print(name.split("t"))
+# print(name.split("a"))
+
+# words = ["Python", "is", "easy"]
+# print(words)
+# print(" ".join(words))
+
+# file = "program.py"
+
+# print(file.startswith("program"))
+# print(file.endswith(".py"))
+
+# print(len("Python"))
+
+
+print("python".isalpha())
+print("python3".isalnum())
+print("12345".isdigit())
+print("   ".isspace())
+print("Title Case".istitle())
+print("lowercase".islower())
+print("UPPERCASE".isupper())
+
+
+
+
+
