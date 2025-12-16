a4fa4da382ec9fbb38b26bd75cbee4bb69a8840e
Aditya Kumar <adii.utsav@gmail.com>
2025-12-16 16:02:22 +0530

    Studied about tuples in python

---
+++ Day11-20/Day14/main.py
@@ -0,0 +1,19 @@
+# l = (3,4,5)
+# print(l[0])
+# print(l[1])
+
+# l = (5,67,8)
+# l[0] = 45
+# print(l)
+
+# l = (3,4,5,"Aditya")
+# print(len(l))
+
+# l = (3,4,5,"Aditya",True,5.6)
+# if 4 in l:
+#     print("Yes")
+
+l = (3,4,5,6)
+m = l[1:2]
+print(l)
+print(m)
