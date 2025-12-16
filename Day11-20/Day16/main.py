706245682c68eeb90308deeb81e8ed14958ef96c
Aditya Kumar <adii.utsav@gmail.com>
2025-12-16 19:19:02 +0530

    Studied about f-strings in python

---
+++ Day11-20/Day16/main.py
@@ -0,0 +1,15 @@
+# sentence = "Hey there {1}, how is {0}"
+# name = "Aditya"
+# place = "India"
+# formatted_sentence = sentence.format(name, place)
+# print(formatted_sentence)
+
+
+# name = "Aditya"
+# place = "India"
+# sentence = f"Hey there {name}, how is {place}"
+# print(sentence)
+
+print(f"{{4*5}}")
+
+# print(f"{'Aditya':^20}")  # Center aligned within 20 spaces
