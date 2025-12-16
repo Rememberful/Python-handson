fc9fc14962ff7b1be6c1e4d22ea69d6ac020267a
Aditya Kumar <adii.utsav@gmail.com>
2025-12-16 08:18:29 +0530

    Studied about functions

---
+++ Day1-10/Day10/main.py
@@ -0,0 +1,40 @@
+# a = 9
+# b = 8
+# gmean = (a*b)/(a+b)
+# print(gmean)
+
+# def gmean(a, b):
+#     mean = (a * b) / (a + b)
+#     print(mean)
+
+# a = 9
+# b = 8
+# gmean(a,b)
+
+# a = 8
+# b = 9
+# if(a>b):
+#     print("First number is greater")
+# else:
+#     print("Second number is greater")
+
+# def isGreater(a, b):
+#     if(a > b):
+#         print("First number is greater")
+#     else:
+#         print("Second number is greater")
+# c = 8
+# d = 9
+# isGreater(c,d)
+
+# def isLove():
+#     pass
+
+def greet():
+    print("Hello, Welcome to Python")
+greet()
+
+
+
+
+
