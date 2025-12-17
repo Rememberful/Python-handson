f795c2bfaead0e1ce10d9c7e3e7ed246d42e2340
Aditya Kumar <adii.utsav@gmail.com>
2025-12-17 08:07:52 +0530

    Studied about docstrings in python

---
+++ Day11-20/Day17/main.py
@@ -0,0 +1,12 @@
+# def square(n):
+#     '''Prints the square of a number n.'''
+#     print(n*n)
+# square(5)
+# print(square.__doc__)
+# help(square)
+
+def square(n):
+    print(n*n)
+    '''Prints the square of a number n.'''
+square(5)
+help(square)
