3f8c4449c5317b6cdcc9b81d82cc51f391c0e479
Aditya Kumar <adii.utsav@gmail.com>
2025-12-18 12:22:28 +0530

    Studied about os Module in Python

---
+++ Day21-30/Day28/main.py
@@ -0,0 +1,16 @@
+# import os
+# # os.mkdir("data")
+# # print(os.path.exists("data"))
+
+# import os
+# os.rename("data","renamed_data")
+
+# import os
+# print(os.getcwd())
+
+import os
+os.mkdir("test_folder")        # Create one folder
+os.makedirs("folder1/folder2") # Create nested folders
+
+os.rmdir("test_folder")        # Remove one folder
+os.removedirs("folder1/folder2") # Remove nested folders
