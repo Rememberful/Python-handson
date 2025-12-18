cd54331fdf60d8d847048a6dc52ffce33000a79d
Aditya Kumar <adii.utsav@gmail.com>
2025-12-18 15:40:10 +0530

    Studied about OOPs introduction, class and object in Python

---
+++ Day31-40/Day33/main.py
@@ -0,0 +1,18 @@
+# class Person:
+#     name = "Harry"
+#     occupation = "SDE"
+#     salary = 10
+# a = Person()
+# a.name = "Utsav"
+# print(a.name)
+
+class Car:
+    def start():
+        print("Car started")
+    def stop():
+        print("Car stopped")
+car = Car()
+car.start()
+car.stop()
+
+
