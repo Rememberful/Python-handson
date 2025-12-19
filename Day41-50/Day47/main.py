3c6bec0a040c9ebc26a0d7713b6aeb8ee7b6a44d
Aditya Kumar <adii.utsav@gmail.com>
2025-12-19 10:14:33 +0530

    Studied about walrus operator in Python

---
+++ Day41-50/Day47/main.py
@@ -0,0 +1,21 @@
+# a = True
+# print(a:= False)
+
+# numbers = [1, 2, 3, 4, 5]
+# total = len(numbers)
+# if total > 3:
+#     print(f"List is long ({total} items)")
+
+# numbers = [1, 2, 3, 4, 5]
+# if (total := len(numbers)) > 3:
+#     print(f"List is long ({total} items)")
+
+# if (n := int(input("Enter a number: "))) > 10:
+#     print(f"{n} is greater than 10")
+
+n = int(input("Enter a number: "))
+if n > 10:
+    print(f"{n} is greater than 10")
+
+
+
